24f02288ced8d54d0c23141453c3fe673f95d5af
Neel K <kneel1292@yahoo.com>
2019-10-15 11:00:43 +0000

    btree.c template

---
+++ src/btree.c
@@ -0,0 +1,39 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "btree.h"
+
+
+int btree_open(Btree *s, char *filename, int32_t block_size,
+                 int32_t max_depth,
+                 int do_create, int64_t offset)
+{
+
+    
+
+}
+
+
+int btree_insert(Btree *s, int64_t key, int64_t value)
+{
+
+    
+
+}
+
+
+int btree_search(Btree *s, int64_t key, int64_t *value)
+{
+
+    
+
+}
+
+
+int btree_close(Btree *s)
+{
+
+    
+
+}
