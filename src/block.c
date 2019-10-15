a164d1c8b5b9a8292dccde15e13cc62ae5def5b3
Neel K <kneel1292@yahoo.com>
2019-10-15 11:10:23 +0000

    block.c template

---
+++ src/block.c
@@ -0,0 +1,29 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "block.h"
+
+
+int block_read(Block *s, FILE *fp, int64_t offset)
+{
+
+    
+
+}
+
+
+int block_write(Block *s, FILE *fp, int64_t offset)
+{
+
+    
+
+}
+
+
+int block_append(Block *s, FILE *fp)
+{
+
+    
+
+}
