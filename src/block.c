c27f559fc0b164e636fdcd5537f119a80d917a53
Neel K <kneel1292@yahoo.com>
2019-10-15 11:18:18 +0000

    Docstring for block_read()

--- src/block.c
+++ src/block.c
@@ -8,6 +8,13 @@
 int block_read(Block *s, FILE *fp, int64_t offset)
 {
 
+    /*
+        s: Pointer to an allocated Block where the read data will be
+           stored. Block size will be taken from s->block_size
+        fp: File pointer to file from where block must be read
+        offset: Offset in the file from where the block should be read
+    */
+
     
 
 }
